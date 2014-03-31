7c3009614c3f109d6933f151bd164a6e4ea1624f
Shea Levy <shea@shealevy.com>
2014-03-31 03:26:22 -0400

    Use buildLocalCabal from upstream

--- default.nix
+++ default.nix
@@ -1,8 +1,7 @@
 { pkgs ? import <nixpkgs> {}
 , src ? ./. # Eventually want to filter out ignores
-, zalora-lib ? import <zalora-nix-lib> { inherit pkgs; }
 }:
 
 {
-  build = zalora-lib.buildLocalCabal src "sproxy-web";
+  build = pkgs.haskellPackages.buildLocalCabal src "sproxy-web";
 }
