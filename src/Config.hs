4b35f132beffb201e51a395560581c1245b2cdc0
Sönke Hahn <soenke.hahn@zalora.com>
2014-12-09 18:18:49 +0800

    using sproxy-web.config by default

--- src/Config.hs
+++ src/Config.hs
@@ -7,15 +7,15 @@ import Data.Configurator as C
 import HFlags
 import System.IO.Unsafe (unsafePerformIO)
 
-defineFlag "c:config" ("" :: String) "config file"
+defineFlag "c:config" ("sproxy-web.config" :: String) "config file"
 
 -- | Get the connection string and the port
 --   from the config file
 getConfig :: IO (ByteString, Int)
 getConfig = do
     conf    <- C.load [C.Required flags_config]
-    Just cs <- C.lookup conf "db_connection_string"
-    Just p  <- C.lookup conf "port"
+    cs <- C.require conf "db_connection_string"
+    p  <- C.require conf "port"
     return (cs, p)
 
 dbConnectionString :: ByteString
---
+++ test/ConfigSpec.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, TemplateHaskell #-}
+
+module ConfigSpec where
+
+
+import           Control.Exception
+import           Data.String.Interpolate
+import           Data.String.Interpolate.Util
+import           HFlags
+import           System.Directory
+import           System.IO.Temp
+import           Test.Hspec
+
+import           Config
+
+
+spec :: Spec
+spec = do
+  describe "getConfig" $ do
+    it "uses 'sproxy-web.config' as default config file" $ inTempDirectory $ do
+      writeFile "sproxy-web.config" $ unindent [i|
+          db_connection_string = "foo"
+          port = 42
+        |]
+      _ <- $initHFlags "sproxy-web - Web interface to the sproxy permissions database"
+      getConfig `shouldReturn` ("foo", 42)
+
+
+inTempDirectory :: IO a -> IO a
+inTempDirectory action = withSystemTempDirectory "sproxy-web-test" $ \ tempDir ->
+  bracket (start tempDir) stop (const action)
+ where
+  start tempDir = do
+    outerDir <- getCurrentDirectory
+    setCurrentDirectory tempDir
+    return outerDir
+  stop outerDir = do
+    setCurrentDirectory outerDir
